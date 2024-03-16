1d225012b61416871dbb7885b689be605bbf8deb
abhaysagarofficial <lcb2023037@iiitl.ac.in>
2024-03-16 13:33:57 +0530

    Added dark mode to App

--- translate_app/lib/main.dart
+++ translate_app/lib/main.dart
@@ -1,7 +1,192 @@
+// import 'package:flutter/material.dart';
+// import 'package:translate_app/models/language.dart';
+// import 'package:translate_app/services/service.dart';
+// // import 'package:get/get.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// List<Language> list = [
+//   Language(lang: 'es', language: "Spanish"),
+//   Language(lang: 'fr', language: "France"),
+//   Language(lang: 'de', language: "German"),
+//   Language(lang: "it", language: "Italian"),
+//   Language(lang: "nl", language: "Dutch"),
+//   Language(lang: "pt", language: "Portuguese"),
+//   Language(lang: "ru", language: "Russia"),
+//   Language(lang: "ja", language: "Japanese"),
+//   Language(lang: "zh-cn", language: "Chinese-Simplified"),
+//   Language(lang: "zh-tw", language: "Chinese-Traditional"),
+//   Language(lang: "ko", language: "Korean"),
+//   Language(lang: "ar", language: "Arabic"),
+//   Language(lang: "hi", language: "Hindi")
+// ];
+
+// class MyApp extends StatelessWidget {
+//   MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: LanguageTranslator(),
+//     );
+//   }
+// }
+
+// class LanguageTranslator extends StatefulWidget {
+//   const LanguageTranslator({super.key});
+
+//   @override
+//   State<LanguageTranslator> createState() => _LanguageTranslatorState();
+// }
+
+// class _LanguageTranslatorState extends State<LanguageTranslator> {
+//   final _input = TextEditingController();
+
+//   String lang = list.first.lang;
+
+//   String result = "";
+
+//   bool isVisible = true;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Translator"),
+//         centerTitle: true,
+//         elevation: 0,
+//         backgroundColor: const Color.fromARGB(255, 44, 37, 230),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Container(
+//           margin: const EdgeInsets.symmetric(horizontal: 15),
+//           child: Column(
+//             children: <Widget>[
+//               SizedBox(
+//                 height: MediaQuery.of(context).size.height / 4,
+//               ),
+//               TextField(
+//                 decoration: InputDecoration(
+//                     hintText: 'Enter the text',
+//                     border: OutlineInputBorder(
+//                         borderRadius: BorderRadius.circular(5))),
+//                 controller: _input,
+//                 onChanged: (value) {
+//                   _input.text = value;
+//                   print(_input.text);
+//                 },
+//               ),
+//               const SizedBox(
+//                 height: 10,
+//               ),
+//               DecoratedBox(
+//                 decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.circular(5),
+//                     border:
+//                         Border.all(color: const Color.fromARGB(136, 60, 60, 60))),
+//                 child: Padding(
+//                   padding:
+//                       const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+//                   child: DropdownButton(
+//                     value: lang,
+//                     items: list.map<DropdownMenuItem<String>>((Language value) {
+//                       return DropdownMenuItem<String>(
+//                         value: value.lang,
+//                         child: Text(value.language),
+//                       );
+//                     }).toList(),
+//                     onChanged: (value) {
+//                       setState(() {
+//                         lang = value!;
+//                       });
+//                       print(lang);
+//                     },
+//                     underline: Container(), //remove underline
+//                     isExpanded: true,
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 10,
+//               ),
+//               SizedBox(
+//                 width: double.infinity,
+//                 height: 50,
+//                 child: TextButton(
+//                   onPressed: () async {
+//                     setState(() {
+//                       isVisible = false;
+//                     });
+//                     FocusScope.of(context).unfocus();
+//                     var result1 =
+//                         await Service().translateText(_input.text, lang);
+//                     setState(() {
+//                       result = result1;
+//                       isVisible = true;
+//                     });
+//                   },
+//                   style: ButtonStyle(
+//                     backgroundColor: MaterialStateProperty.all<Color>(
+//                         const Color.fromARGB(255, 44, 37, 230)),
+//                     foregroundColor:
+//                         MaterialStateProperty.all<Color>(Colors.white),
+//                   ),
+//                   child: isVisible
+//                       ? const Text(
+//                           "Convert",
+//                           style: TextStyle(fontSize: 20),
+//                         )
+//                       : const CircularProgressIndicator(
+//                           color: Colors.white,
+//                         ),
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               Visibility(
+//                 visible: result.isNotEmpty,
+//                 child: Column(
+//                   children: [
+//                     const Text(
+//                       "Result",
+//                       style: TextStyle(fontSize: 20, color: Colors.green),
+//                     ),
+//                     const SizedBox(
+//                       height: 8,
+//                     ),
+//                     Text(
+//                       result,
+//                       style: const TextStyle(fontSize: 18),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:translate_app/models/language.dart';
 import 'package:translate_app/services/service.dart';
-// import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,45 +209,44 @@ List<Language> list = [
 ];
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light, // Default theme is light mode
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark, // Dark mode theme
+      ),
       home: LanguageTranslator(),
     );
   }
 }
 
 class LanguageTranslator extends StatefulWidget {
-  const LanguageTranslator({super.key});
-
   @override
   State<LanguageTranslator> createState() => _LanguageTranslatorState();
 }
 
 class _LanguageTranslatorState extends State<LanguageTranslator> {
   final _input = TextEditingController();
-
   String lang = list.first.lang;
-
   String result = "";
-
   bool isVisible = true;
+  bool isDarkMode = false; // Track dark mode status
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Translator"),
+        title: Text("Translator"),
         centerTitle: true,
         elevation: 0,
-        backgroundColor: const Color.fromARGB(255, 44, 37, 230),
       ),
       body: SingleChildScrollView(
         child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 15),
+          margin: EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             children: <Widget>[
               SizedBox(
@@ -79,17 +263,17 @@ class _LanguageTranslatorState extends State<LanguageTranslator> {
                   print(_input.text);
                 },
               ),
-              const SizedBox(
+              SizedBox(
                 height: 10,
               ),
               DecoratedBox(
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(5),
                     border:
-                        Border.all(color: const Color.fromARGB(136, 60, 60, 60))),
+                        Border.all(color: Color.fromARGB(136, 60, 60, 60))),
                 child: Padding(
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                      EdgeInsets.symmetric(horizontal: 10, vertical: 4),
                   child: DropdownButton(
                     value: lang,
                     items: list.map<DropdownMenuItem<String>>((Language value) {
@@ -109,7 +293,7 @@ class _LanguageTranslatorState extends State<LanguageTranslator> {
                   ),
                 ),
               ),
-              const SizedBox(
+              SizedBox(
                 height: 10,
               ),
               SizedBox(
@@ -130,37 +314,36 @@ class _LanguageTranslatorState extends State<LanguageTranslator> {
                   },
                   style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color.fromARGB(255, 44, 37, 230)),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
+                        Color.fromARGB(255, 44, 37, 230)),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
                   ),
                   child: isVisible
-                      ? const Text(
+                      ? Text(
                           "Convert",
                           style: TextStyle(fontSize: 20),
                         )
-                      : const CircularProgressIndicator(
+                      : CircularProgressIndicator(
                           color: Colors.white,
                         ),
                 ),
               ),
-              const SizedBox(
+              SizedBox(
                 height: 20,
               ),
               Visibility(
                 visible: result.isNotEmpty,
                 child: Column(
                   children: [
-                    const Text(
+                    Text(
                       "Result",
                       style: TextStyle(fontSize: 20, color: Colors.green),
                     ),
-                    const SizedBox(
+                    SizedBox(
                       height: 8,
                     ),
                     Text(
                       result,
-                      style: const TextStyle(fontSize: 18),
+                      style: TextStyle(fontSize: 18),
                     ),
                   ],
                 ),
@@ -169,6 +352,16 @@ class _LanguageTranslatorState extends State<LanguageTranslator> {
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Toggle dark mode
+          setState(() {
+            isDarkMode = !isDarkMode;
+          });
+        },
+        child: Icon(Icons.lightbulb_outline),
+      ),
     );
   }
 }
+
