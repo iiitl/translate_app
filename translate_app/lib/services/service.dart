6ee02810216c1bd6e7dfe3b789c71449217dd7fa
gauravghodinde <gauravghodinde@gmail.com>
2024-03-17 04:04:32 +0530

    updated the api and response

--- translate_app/lib/services/service.dart
+++ translate_app/lib/services/service.dart
@@ -3,19 +3,43 @@ import 'dart:convert';
 
 class Service {
   Future<String> translateText(String text, String lang) async {
-    String url = "https://todoapp-7jzr.onrender.com/translate/translateLang";
-    final response = await http.post(Uri.parse(url),
-        body: {"text": text, "targetLang": lang, "sourceLang": "en"});
+    // String key = ""
+    final headers = {
+      'accept': 'application/json',
+      'Content-Type': 'application/json',
+    };
+
+    final data =
+        '{\n  "source": "en",\n  "target": "es",\n  "text": "hello",\n  "proxies": []\n}';
+
+    final url =
+        Uri.parse('https://deep-translator-api.azurewebsites.net/mymemory/');
+
+    final response = await http.post(url, headers: headers, body: data);
+    // String url =
+    //     "https://deep-translator-api.azurewebsites.net/mymemory/";
+    // final response = await http.post(Uri.parse(url), headers: {
+    //   'X-RapidAPI-Key': 'c31066519fmsh127a08d5258a9c3p1f3d85jsn61dd90156031',
+    //   'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+    // }, body: {
+    //   "source": "auto",
+    //   "target": lang,
+    //   "text": text,
+    // });
+    print("printing resposnse");
+    print(response.statusCode.toString());
     var result;
     if (response.statusCode == 200) {
       var answer = json.decode(response.body);
+      print("answer");
       print(answer);
-      if (answer['success'] == true) {
-        answer = answer['message']!['data']['translations'];
-        result = answer![0]['translatedText'].toString();
+      if (answer['error'] == null) {
+        answer = answer['translation'];
+        result = answer.toString();
       } else {
         result = "Something went wrong";
       }
+      print("result");
       print(result);
     } else {
       result = "Something went wrong";
