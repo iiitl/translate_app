e5b84e46fd930f77f7c32d08f4d3c7078127323c
Ashutoshdapakara <ashutoshdapakara0@gmail.com>
2024-03-16 19:12:45 +0530

    fixed text field input issue

--- translate_app/lib/main.dart
+++ translate_app/lib/main.dart
@@ -75,8 +75,11 @@ class _LanguageTranslatorState extends State<LanguageTranslator> {
                         borderRadius: BorderRadius.circular(5))),
                 controller: _input,
                 onChanged: (value) {
-                  _input.text = value;
+                  
+                  setState(() {
+                    
                   print(_input.text);
+                  });
                 },
               ),
               const SizedBox(
@@ -85,8 +88,8 @@ class _LanguageTranslatorState extends State<LanguageTranslator> {
               DecoratedBox(
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(5),
-                    border:
-                        Border.all(color: const Color.fromARGB(136, 60, 60, 60))),
+                    border: Border.all(
+                        color: const Color.fromARGB(136, 60, 60, 60))),
                 child: Padding(
                   padding:
                       const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
